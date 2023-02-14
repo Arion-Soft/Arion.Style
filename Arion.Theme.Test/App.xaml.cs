66b7579854e04eedd7b19019698f2fd89dca952f
IvanBudanov <ivanderk2003@gmail.com>
2023-02-14 17:22:34 +0300

    Push

---
+++ Arion.Theme.Test/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Arion.Theme.Test
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Arion.Theme.Test/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Arion.Theme.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
