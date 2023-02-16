eb227cc534faa812f0ed2fd38439a7d1c8e77914
IvanBudanov <ivanderk2003@gmail.com>
2023-02-16 18:31:46 +0300

    v0.0.5

--- Arion.Theme.Test/MainWindow.xaml.cs
+++ Arion.Theme.Test/MainWindow.xaml.cs
@@ -17,21 +17,123 @@ namespace Arion.Theme.Test
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
         }
-
+        
         private bool altPressed;
         private bool keyUp;
         private bool keyDown;
         private bool keyLeft;
         private bool keyRight;
-
-        private void MainWindow_OnKeyDown(object sender, KeyEventArgs e)
+        
+        private void MainWindow_OnPreviewKeyDown(object sender, KeyEventArgs e)
         {
-            
+            if (e.Key == Key.RightAlt)
+            {
+                altPressed = true; CbxAlt.IsChecked = true;
+            }
+
+            if (e.Key == Key.Up)
+            {
+                keyUp = true; CbxUp.IsChecked = true;
+            }
+
+            if (e.Key == Key.Down)
+            {
+                keyDown = true; CbxDown.IsChecked = true;
+            }
+
+            if (e.Key == Key.Left)
+            {
+                keyLeft = true; CbxLeft.IsChecked = true;
+            }
+
+            if (e.Key == Key.Right)
+            {
+                keyRight = true; CbxRight.IsChecked = true;
+            }
         }
 
-        private void MainWindow_OnKeyUp(object sender, KeyEventArgs e)
+        private void MainWindow_OnPreviewKeyUp(object sender, KeyEventArgs e)
         {
-            
+            if (e.Key == Key.RightAlt)
+            {
+                altPressed = false; CbxAlt.IsChecked = false;
+            }
+
+            if (e.Key == Key.Up)
+            {
+                keyUp = false; CbxUp.IsChecked = false;
+            }
+
+            if (e.Key == Key.Down)
+            {
+                keyDown = false; CbxDown.IsChecked = false;
+            }
+
+            if (e.Key == Key.Left)
+            {
+                keyLeft = false; CbxLeft.IsChecked = false;
+            }
+
+            if (e.Key == Key.Right)
+            {
+                keyRight = false; CbxRight.IsChecked = false;
+            }
+        }
+
+        private void UIElement_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.RightAlt)
+            {
+                altPressed = true; CbxAlt.IsChecked = true;
+            }
+
+            if (e.Key == Key.Up)
+            {
+                keyUp = true; CbxUp.IsChecked = true;
+            }
+
+            if (e.Key == Key.Down)
+            {
+                keyDown = true; CbxDown.IsChecked = true;
+            }
+
+            if (e.Key == Key.Left)
+            {
+                keyLeft = true; CbxLeft.IsChecked = true;
+            }
+
+            if (e.Key == Key.Right)
+            {
+                keyRight = true; CbxRight.IsChecked = true;
+            }
+        }
+
+        private void UIElement_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.RightAlt)
+            {
+                altPressed = false; CbxAlt.IsChecked = false;
+            }
+
+            if (e.Key == Key.Up)
+            {
+                keyUp = false; CbxUp.IsChecked = false;
+            }
+
+            if (e.Key == Key.Down)
+            {
+                keyDown = false; CbxDown.IsChecked = false;
+            }
+
+            if (e.Key == Key.Left)
+            {
+                keyLeft = false; CbxLeft.IsChecked = false;
+            }
+
+            if (e.Key == Key.Right)
+            {
+                keyRight = false; CbxRight.IsChecked = false;
+            }
         }
     }
 }
