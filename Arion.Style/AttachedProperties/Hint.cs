c4c4b032c800f09be744240b6bba94074655e767
IvanBudanov <ivanderk2003@gmail.com>
2023-03-29 09:09:28 +0300

    v0.3.0.7

--- Arion.Style/AttachedProperties/Hint.cs
+++ Arion.Style/AttachedProperties/Hint.cs
@@ -16,5 +16,18 @@ namespace Arion.Style.AttachedProperties
         {
             return (string)element.GetValue(PlaceholderProperty);
         }
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+            "CornerRadius", typeof(CornerRadius), typeof(Hint), new PropertyMetadata(default(CornerRadius)));
+
+        public static void SetCornerRadius(DependencyObject element, CornerRadius value)
+        {
+            element.SetValue(CornerRadiusProperty, value);
+        }
+
+        public static CornerRadius GetCornerRadius(DependencyObject element)
+        {
+            return (CornerRadius)element.GetValue(CornerRadiusProperty);
+        }
     }
 }
