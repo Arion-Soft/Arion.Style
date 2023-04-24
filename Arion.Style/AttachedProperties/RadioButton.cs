fe5bb3706d338bf55dd60e05e06d671522b93ff7
IvanBudanov <ivanderk2003@gmail.com>
2023-04-24 16:43:02 +0300

    Update

---
+++ Arion.Style/AttachedProperties/RadioButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Arion.Style.AttachedProperties
+{
+    public class RadioButton
+    {
+        #region Description
+
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.RegisterAttached(
+            "Description", typeof(string), typeof(RadioButton), new PropertyMetadata(default(string)));
+
+        public static void SetDescription(DependencyObject element, string value)
+        {
+            element.SetValue(DescriptionProperty, value);
+        }
+
+        public static string GetDescription(DependencyObject element)
+        {
+            return (string)element.GetValue(DescriptionProperty);
+        }
+
+        #endregion
+    }
+}
---
+++ Arion.Theme.Test/Views/DocumentViewer.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Xps.Packaging;
+
+namespace Arion.Theme.Test.Views
+{
+    public partial class DocumentViewer : UserControl
+    {
+        public DocumentViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void DocumentViewer_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            string fileName = null;
+            string appPath = System.IO.Path.GetDirectoryName(Assembly.GetAssembly(typeof(App)).CodeBase);
+            fileName = appPath + @"\Files\Instruction.xps";
+            fileName = fileName.Remove(0, 6);
+            XpsDocument doc = new XpsDocument(fileName, FileAccess.Read);
+            DcViewer.Document = doc.GetFixedDocumentSequence();
+        }
+    }
+}
