8c0fe187428712f9d7311d0ed9ebcff27c16a624
IvanBudanov <ivanderk2003@gmail.com>
2023-05-04 16:53:05 +0300

    fix

---
+++ Arion.Style/AttachedProperties/TabItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Arion.Style.AttachedProperties
+{
+    public class TabItem
+    {
+        public static readonly DependencyProperty MainBorderProperty = DependencyProperty.RegisterAttached(
+            "MainBorder", typeof(Thickness), typeof(TabItem), new PropertyMetadata(default(Thickness)));
+
+        public static void SetMainBorder(DependencyObject element, Thickness value)
+        {
+            element.SetValue(MainBorderProperty, value);
+        }
+
+        public static Thickness GetMainBorder(DependencyObject element)
+        {
+            return (Thickness)element.GetValue(MainBorderProperty);
+        }
+    }
+}
