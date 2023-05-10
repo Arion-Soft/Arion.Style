423fe14c0d7e829a0a098a40f33597213d8bbfea
IvanBudanov <ivanderk2003@gmail.com>
2023-05-10 11:17:30 +0300

    Обновлен стиль для GroupBox

---
+++ Arion.Style/AttachedProperties/GroupBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Arion.Style.AttachedProperties
+{
+    public class GroupBox
+    {
+        public static readonly DependencyProperty BackgroundHeaderProperty = DependencyProperty.RegisterAttached(
+            "BackgroundHeader", typeof(SolidColorBrush), typeof(GroupBox), new PropertyMetadata(default(SolidColorBrush)));
+
+        public static void SetBackgroundHeader(DependencyObject element, SolidColorBrush value)
+        {
+            element.SetValue(BackgroundHeaderProperty, value);
+        }
+
+        public static SolidColorBrush GetBackgroundHeader(DependencyObject element)
+        {
+            return (SolidColorBrush)element.GetValue(BackgroundHeaderProperty);
+        }
+    }
+}
