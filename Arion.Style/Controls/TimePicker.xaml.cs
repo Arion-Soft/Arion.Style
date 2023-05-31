1deadd42e66160ed49060f9b8cd59cd7b49a6f86
IDerkBot <ivanderk2003@gmail.com>
2023-05-31 21:58:36 +0300

    Update 0.5.4.5

---
+++ Arion.Style/Controls/TimePicker.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Arion.Style.Controls
+{
+    public partial class TimePicker : UserControl
+    {
+        public TimePicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
