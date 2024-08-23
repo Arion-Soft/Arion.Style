90c3ab6d7b975db777996db90b8b09a5906e5d14
Valery Noskov <ven@ari-on.ru>
2024-08-23 14:48:22 +0300

    feature(#1058): added converter for grid column size and double

---
+++ Arion.Style/Converters/DoubleGridLengthConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Arion.Style.Converters;
+
+public class DoubleGridLengthConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        return new GridLength((double)value);
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        GridLength gridLength = (GridLength)value;
+        return gridLength.Value;
+    }
+}
